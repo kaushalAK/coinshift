447ccd04aa4dca72acd689495004a9a55b2b7d65
abhishekkaushal <abhishek.kaushal@cred.club>
2022-04-17 10:43:58 +0530

    added support to get data using blockNumber

--- routes/blockTransaction.js
+++ routes/blockTransaction.js
@@ -9,15 +9,13 @@ const blockRouter = express.Router();
 
 class BlockTransactionChecker {
     web3;
-    account;
   
-    constructor(projectId, account) {
+    constructor(projectId) {
       this.web3 = new Web3(
         new Web3.providers.HttpProvider(
           "https://rinkeby.infura.io/v3/" + projectId
         )
       );
-      this.account = account;
     }
   
       async checkBlock() {
@@ -89,18 +87,138 @@ class BlockTransactionChecker {
     }
   }
   
-  blockRouter.post("/block/transactions", (req, res) => {
+  blockRouter.post("/block/transactions/all", (req, res) => {
     const data = {
-      account: req.body.account,
       projectId: req.body.processID,
     };
-    let txChecker = new BlockTransactionChecker(data.projectId, data.account);
+    let txChecker = new BlockTransactionChecker(data.projectId);
     setInterval(() => {
         txChecker.checkBlock();
     }, 15*1000)
     
-    res.json({ status: "Started Downloading and Data Dump" });
+    res.json({ status: "Started Downloading and Data Dump every 15 seconds" });
   });
+
+
+  class FetchBlockTransaction{
+    web3;
+  
+    constructor(projectId, blockNumber) {
+      this.web3 = new Web3(
+        new Web3.providers.HttpProvider(
+          "https://rinkeby.infura.io/v3/" + projectId
+        )
+      );
+      this.blockNumber = blockNumber;
+    }
+
+    async checkBlock() {
+        let block = await this.web3.eth.getBlock(this.blockNumber);
+        let number = block.number;
+        console.log("Searching block controller " + number);
+        this.persistBlockData(block);
+        console.log("BLock persisted");
+        if (block != null && block.transactions != null) {
+          for (let txHash of block.transactions) {
+            try {
+              let tx = await this.web3.eth.getTransactionCount(txHash);
+              this.persistData(tx);
+            } catch (err) {
+              continue;
+            }
+          }
+        }
+      }
+
+      persistData(transaction) {
+        const data = {
+          txnHash: transaction.hash,
+          blockHash: transaction.blockHash,
+          fromAddress: transaction.from,
+          toAddress: transaction.to,
+          txnIndex: transaction.transactionIndex,
+          value: transaction.value,
+          gas: transaction.gas,
+          gasPrice: transaction.gasPrice,
+          block_number: transaction.blockNumber,
+          type: transaction.type,
+        };
+    
+        const query =
+          "INSERT INTO block_transactions_entity VALUES (?,?,?,?,?,?,?,?,?,?)";
+    
+        pool.query(query, Object.values(data), (error) => {
+          if (error) {
+            console.log("Error -->" + error);
+          } else {
+            console.log("Success");
+          }
+        });
+      }
+    
+      persistBlockData(block) {
+        const data = {
+          blockHash: block.hash,
+          blockNumber: block.number,
+          blockSize: block.size,
+          timestamp: block.timestamp,
+          totalDifficulty: block.totalDifficulty,
+          difficulty: block.difficulty,
+          gasUsed: block.gasUsed,
+          gasLimit: block.gasLimit,
+        };
+    
+        const query = "INSERT INTO block_entity VALUES (?,?,?,?,?,?,?,?)";
+    
+        pool.query(query, Object.values(data), (error) => {
+          if (error) {
+            console.log("Error -->" + error);
+          } else {
+            console.log("Success");
+          }
+        });
+      }
+  }
+
+  blockRouter.post("/fetch/block/transactions", (req, res) => {
+    const data = {
+      projectId: req.body.processID,
+      blockNumber: req.body.blockNumber,
+    };
+    let txChecker = new FetchBlockTransaction(data.projectId, data.blockNumber);
+        txChecker.checkBlock();
+    res.json({ status: "Started Downloading and Data Dump" });
+});
+
+
+blockRouter.post("/block/transactions/blockNumber", (req,res) => {
+    const data = {
+        projectId: req.body.processID,
+        blockNumber: req.body.blockNumber,
+      };
+    const query =
+    "SELECT * from block_transactions_entity where block_hash = (SELECT block_hash from block_entity where block_number = '" +
+    data.blockNumber +
+    "')";
+    console.log('query--> '+query)
+    pool.query(query, Object.values(data), (error, rows) => {
+        console.log(rows)
+        if (rows && rows.length ==0) {
+          var blockChecker = new FetchBlockTransaction(data.projectId, data.blockNUmber);
+          blockChecker.checkBlock();
+          console.log("No Records found.. Started data dump...");
+        }
+        if(rows && rows.length ==0){
+            res.json({ Status: "No Data found. Getting data" });
+            
+        }else res.json({ data: rows });
+        
+        if (error) {
+          res.json({ status: "failed to get details" });
+        }
+      });
+})
+    
 
   const pool = mysql.createConnection({
     host     : process.env.DB_HOST,
